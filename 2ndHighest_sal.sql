4d5f3e73385e226f4a9ca6d7010771cc8bc56393
tuhinmanna <42709485+tuhinmanna@users.noreply.github.com>
2025-11-10 16:02:04 +0530

    [Updated] Implement second highest salary queries
    Added multiple methods to find the second highest salary.

--- 2ndHighest_sal.sql
+++ 2ndHighest_sal.sql
@@ -7,3 +7,19 @@ order by salary desc limit 1 offset 1;
 
 select max(salary) from skewed_employees
 where salary < ( select max(salary) from skewed_employees);
+
+
+## With window function
+
+
+with cte as (
+select salary, dense_rank() over(order by salary desc) as rnk 
+from Employee
+)
+select ifnull(select salary from cte where rnk = 2 limit 1, null) as SecondHighestSalary 
+
+## Lowest runtime
+
+select max(salary) as SecondHighestSalary
+from employee
+where salary not in (select max(salary) from employee)
